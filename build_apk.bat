fba61e88b5d741d148a23de01ea877dd28189afd
Jesse Li-Yates <jjjegly@gmail.com>
2026-04-12 19:17:49 +1000

    Delete build_apk.bat

--- build_apk.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set JAVA_HOME=C:\Program Files\Android\Android Studio\jbr
-set ANDROID_HOME=%USERPROFILE%\AppData\Local\Android\Sdk
-set PATH=%JAVA_HOME%\bin;%PATH%
-echo JAVA_HOME=%JAVA_HOME%
-echo ANDROID_HOME=%ANDROID_HOME%
-java -version
-call gradlew.bat assembleDebug
-echo BUILD EXIT CODE: %ERRORLEVEL%
